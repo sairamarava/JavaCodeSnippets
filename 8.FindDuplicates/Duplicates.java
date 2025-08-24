0b74a330b996c941b49f746f9b9a3e18d5d6903e
sairamarava <sairamrdya@gmail.com>
2025-08-24 22:19:43 +0530

    changed a few

--- 8.FindDuplicates/Duplicates.java
+++ 8.FindDuplicates/Duplicates.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class Duplicates {
     public static void main(String[] args) {
-        Map <Character, Integer> freq= new HashMap<>();
+        Map<Character, Integer> freq= new HashMap<>();
         String input="sai ram reddy";
         for(char ch: input.toCharArray()){
             freq.put(ch,freq.getOrDefault(ch, 0)+1);
         }
-        for(Map.Entry <Character, Integer> e : freq.entrySet()){
-            if(e.getValue()>1){
+        for(Map.Entry<Character, Integer> e : freq.entrySet()){
+            if(e.getValue()>1 && e.getKey()!=' '){
                 System.out.print(e.getKey()+"->"+e.getValue()+" ");
             }
         }
