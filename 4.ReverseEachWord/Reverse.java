ba57c47daedd3a68f6bc74bb78496da73f52ac1c
sairamarava <sairamrdya@gmail.com>
2025-08-30 22:09:00 +0530

    practiced more code

--- 4.ReverseEachWord/Reverse.java
+++ 4.ReverseEachWord/Reverse.java
@@ -1,4 +1,13 @@
 public class Reverse {
+    static void reverse(String str){
+        String[] words=str.split(" ");
+        StringBuilder res=new StringBuilder();
+        for(String word: words){
+            res.append(new StringBuilder(word).reverse().toString());
+            res.append(" ");
+        }
+        System.out.println(res.toString());
+    }
     public static void main(String[] args) {
         String input="sairam is a good boy";
         String[] words=input.split(" ");
@@ -8,5 +17,6 @@ public class Reverse {
             result.append(" ");
         }
         System.out.println(result.toString());
+        reverse(input);
     }
 }
